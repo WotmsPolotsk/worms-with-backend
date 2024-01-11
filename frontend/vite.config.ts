ef9851fcb2cb4d823828e329d55aaf523f48d7b8
Nikita Sirodoev <syrodoev9@gmail.com>
2024-01-11 12:53:18 +0300

    Add localhost proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://worms-essr.onrender.com/",
+        target: "http://localhost:3001",
         secure: false,
       },
     },
